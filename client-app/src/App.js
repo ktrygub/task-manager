d4b6b7a075cbe18b0753dd8431462dc88d247394
Ktrygub <konstantin.trygub@gmail.com>
2018-04-01 05:31:20 +0300

    CRA + initial configuration

---
+++ client-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+/* eslint-disable react/prefer-stateless-function */
+
+class App extends Component {
+  render() {
+    return (
+      <p>
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    )
+  }
+}
+
+export default App
